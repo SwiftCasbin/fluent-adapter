83d8e9d41a654248135650655dd4b099cceea3bf
不明不惑 <cit117@126.com>
2021-06-15 14:57:00 +0800

    update to casbin v1.0.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(name:"Casbin",url: "https://github.com/xiaobeiswift/SwiftCasbin.git", .branch("master"))
+        .package(name:"Casbin",url: "https://github.com/casbin/SwiftCasbin.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
